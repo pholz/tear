238110e3da26cae1ede154356327fc1933408e8c
pholz <p.holzkorn@gmail.com>
2010-10-27 23:59:49 -0400

    wed nite

--- src/tearApp.cpp
+++ src/tearApp.cpp
@@ -160,6 +160,7 @@ void tearApp::update()
 	float dt = now - last;
 	last = now;
 	
+	// move corners according to tug (indirectly and directly)
 	for(int i = 0; i < 4; i++)
 	{
 		if(tug[i] > .0f) tug[i] *= .93f;
@@ -178,6 +179,7 @@ void tearApp::update()
 	centroid->x = .0f;
 	centroid->y = .0f;
 	
+	// calc entroid
 	for(int i = 0; i < 4; i++)
 	{
 		*centroid += blob->getPoints()[i];
@@ -187,11 +189,13 @@ void tearApp::update()
 	
 	float sumdist = .0f;
 	
+	// get sum of distances of corner points to centroid
 	for(int i = 0; i < 4; i++)
 	{
 		sumdist += blob->getPoints()[i].distance(*centroid);
 	}
 	
+	// add "elastic" force to corners, proportional to "tension" in the system
 	for(int i = 0; i < 4; i++)
 	{
 		Vec2f& pt = blob->getPoints()[i];
@@ -203,8 +207,10 @@ void tearApp::update()
 		}
 	}
 	
+	// update enemy generator
 	egen->update(dt);
 	
+	// collision detection and reaction between enemies and blob
 	colliding = egen->collideWithBlob();
 	
 	if(colliding){
@@ -217,6 +223,7 @@ void tearApp::update()
 	}
 	
 	
+	// advance "history" of tugs per wiimote
 	for(int j = 0; j < 4; j++)
 	{
 		for(int i = 0; i < 499; i++)
@@ -231,6 +238,8 @@ void tearApp::update()
 		if(tugctr[j] < .0f) tugctr[j] = .0f;
 		
 		tugged[j] = hasTugged(j);
+		
+		// add tug force if a tug has been detected
 		if(tugged[j]) tug[j] += TUGSC * 3;
 	}
 	
@@ -246,32 +255,12 @@ void tearApp::update()
 
 bool tearApp::hasTugged(int id)
 {
+	// find a minimum, then move to right and left to search for maxima
+	// if all has been found, and the "spike" is big enough, and enough time has passed since
+	// the last detection, detect a tug
+	
 	float max1 = .0f, min = .0f, max2 = .0f;
 	int mindex = 0;
-	
-	/*
-	for(int i = 498; i >= 430; i--)
-	{
-		if(math<float>::abs(max1 - min) > 30.0f) spikefound = true;
-		
-		if(!spikefound){
-			max1 = math<float>::max(max1, wiitug[i]);
-			min = math<float>::min(min, wiitug[i]);
-		} else {
-			if(wiitug[i] < wiitug[i+1] - 3.0f) break;
-			max2 = math<float>::max(max2, wiitug[i]);
-		}
-	}
-	
-	if(tugctr == .0f && max1 > .0f && max2 > .0f && min < -10.0f && spikefound && math<float>::abs(max1-max2) < 10.0f && math<float>::abs(max1-min) > 25.0f)
-	{
-		tugctr = 1.0f;
-		return true;
-		
-	}
-	else
-		return false;
-	 */
 	
 	for(int i = 450; i < 500; i++)
 	{
@@ -292,6 +281,7 @@ bool tearApp::hasTugged(int id)
 			max2 = math<float>::max(max1, wiitug[id][i]);
 		}
 		
+		
 		if(tugctr[id] == .0f && max1 > .0f && max2 > .0f && math<float>::abs(max1-max2) < 20.0f)
 		{
 			tugctr[id] = 1.0f;
@@ -308,11 +298,13 @@ void tearApp::draw()
 {
 	gl::enableAlphaBlending( false );
 	
+	// set camera to center on centroid
 	cam.lookAt(Vec3f(centroid->x - getWindowWidth()/2, centroid->y - getWindowHeight()/2, 10.0f), Vec3f(centroid->x - getWindowWidth()/2, centroid->y - getWindowHeight()/2, .0f));
 	gl::setMatrices(cam);
 	
 	gl::clear( Color( 0, 0, 0 ) ); 
 	
+	// draw the background grid, total -10000 to 10000, but clip everything outside the frame
 	for(int i = 0; i < 100; i++)
 		for(int j = 0; j < 100; j++)
 		{
@@ -326,11 +318,11 @@ void tearApp::draw()
 			glPopMatrix();
 		}
 	
-	// clear out the window with black
-	
+	// draw blob lines
 	gl::color(Color(1.0f, .0f, .0f));
 	gl::draw(*blob);
 	
+	// draw corners
 	for(int i = 0; i < 4; i++)
 	{
 		Vec2f& pt = blob->getPoints()[i];
@@ -350,6 +342,7 @@ void tearApp::draw()
 	
 	glPushMatrix();
 	
+	// draw centroid
 	gl::translate(*centroid);
 	
 	gl::color(Color(1.0f, .0f, .0f));
@@ -357,8 +350,10 @@ void tearApp::draw()
 	
 	glPopMatrix();
 	
+	// draw enemies
 	egen->draw();
 	
+	// draw collision indicators
 	if(colliding){
 		vector<Enemy>::iterator it;
 		for(it = colliding->begin(); it < colliding->end(); it++)
@@ -373,6 +368,7 @@ void tearApp::draw()
 		}
 	}
 	
+	// draw debug wiimote data feeds
 	if(debug)
 	{
 		for(int k = 0; k < 4; k++)
@@ -389,6 +385,7 @@ void tearApp::draw()
 		}
 	}
 	
+	// draw scores
 	glColor3f( 1.0f, 1.0f, 1.0f );
 	gl::draw( scoreTexture, Vec2f( cam.getEyePoint().x + 10, cam.getEyePoint().y + getWindowHeight() - scoreTexture.getHeight() - 5 ) );
 	
